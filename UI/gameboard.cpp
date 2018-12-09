ca30c4dc873a40c9ff39d4f6d7cf77674c9e5e89
Petrus Eskelinen <eskeline@linux-desktop2.cc.tut.fi>
2018-12-09 14:52:11 +0200

    testiline poistettu

--- UI/gameboard.cpp
+++ UI/gameboard.cpp
@@ -740,11 +740,6 @@ void GameBoard::movePawn(int pawnId, Common::CubeCoordinate pawnCoord)
                     pawnSlots_.at(pawnCoord).at(2) = true;
                     pawnItems_.at(pawnId)->set_pawnSlot(3);
                 }
-
-                //transportin toiminnan testaukseen
-                if(target_hex->getTransports().size() != 0) {
-                    addPawnToTransport(pawnId, target_hex->getTransports().at(0)->getId());
-                }
                 scene_->update();
             }
         }

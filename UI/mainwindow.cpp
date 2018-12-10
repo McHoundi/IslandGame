375cbbf515824f8b1449f8c3e7c11f8cce0b0a96
Osku Karjalainen <osku.karjalainen@student.tut.fi>
2018-12-10 11:08:14 +0200

    actor ja trasport skip turn bugi korjattu

--- UI/mainwindow.cpp
+++ UI/mainwindow.cpp
@@ -274,7 +274,8 @@ void MainWindow::hex_chosen(std::shared_ptr<Common::Hex> hexi)
                     animalMovesLeft_ -= distance;
                 }
 
-                if (animalMovesLeft_ <= 0 && coords == highlightedActor_->getHex()->getCoordinates()) {
+                if ((animalMovesLeft_ == 0 || animalMovesLeft_ < -99)
+                        && coords == highlightedActor_->getHex()->getCoordinates()) {
                     highlightedActor_ = nullptr;
                     highlightedHex_ = nullptr;
                     wheelSpinned_ = false;
@@ -315,10 +316,15 @@ void MainWindow::hex_chosen(std::shared_ptr<Common::Hex> hexi)
             std::cout << moves << std::endl; // Debug
             try {
                 runner_->moveTransportWithSpinner(highlightedHex_->getCoordinates(), coords, highlightedTransport_->getId(), moves);
-                unsigned int distance = cubeCoordinateDistance(highlightedHex_->getCoordinates(), coords);
-                highlightedHex_ = boardPTR_->get_hexPointers().at(coords);
-                animalMovesLeft_ -= distance;
-                if (animalMovesLeft_ <= 0) {
+
+                if ( coords == highlightedTransport_->getHex()->getCoordinates() ) {
+                    unsigned int distance = cubeCoordinateDistance(highlightedHex_->getCoordinates(), coords);
+                    highlightedHex_ = boardPTR_->get_hexPointers().at(coords);
+                    animalMovesLeft_ -= distance;
+                }
+
+                if ((animalMovesLeft_ == 0 || animalMovesLeft_ < -99) &&
+                        coords == highlightedTransport_->getHex()->getCoordinates()) {
                     highlightedTransport_ = nullptr;
                     highlightedHex_ = nullptr;
                     wheelSpinned_ = false;
@@ -335,7 +341,8 @@ void MainWindow::hex_chosen(std::shared_ptr<Common::Hex> hexi)
                     ui->spinButton->setText("SPIN!!");
                     scene1_->update();
 
-                    std::cout << "next player: " << statePTR_->currentPlayer() << std::endl;
+                    std::string player = std::to_string(statePTR_->currentPlayer());
+                    ui->playervaluelabel->setText(QString::fromStdString(player));
                     statePTR_->changeGamePhase(Common::GamePhase::MOVEMENT);
                     ui->gamephasevaluelabel->setText("MOVEMENT");
                     updateTransportInfo();
